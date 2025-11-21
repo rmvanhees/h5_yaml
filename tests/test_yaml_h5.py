9e27c93eca4f4062587757c791c452a364891d1f
Richard van Hees <r.m.van.hees@sron.nl>
2025-11-21 16:06:44 +0100

    Initialized test-modules for pytest

---
+++ tests/test_yaml_h5.py
@@ -0,0 +1,43 @@
+#
+# This file is part of Python package: `h5yaml`
+#
+#     https://github.com/rmvanhees/pyxarr.git
+#
+# Copyright (c) 2025 - R.M. van Hees (SRON)
+#    All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""Test module for h5yaml class `H5Yaml`."""
+
+from __future__ import annotations
+
+
+class TestH5Yaml:
+    """Class to test H5Yaml from h5yaml.yaml_h5."""
+
+    def test_groups(self: TestH5Yaml) -> None:
+        """..."""
+        return
+
+    def test_dimensions(self: TestH5Yaml) -> None:
+        """..."""
+        return
+
+    def test_compounds(self: TestH5Yaml) -> None:
+        """..."""
+        return
+
+    def test_variables(self: TestH5Yaml) -> None:
+        """..."""
+        return
---
+++ tests/test_yaml_nc.py
@@ -0,0 +1,43 @@
+#
+# This file is part of Python package: `h5yaml`
+#
+#     https://github.com/rmvanhees/pyxarr.git
+#
+# Copyright (c) 2025 - R.M. van Hees (SRON)
+#    All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""Test module for h5yaml class `NcYaml`."""
+
+from __future__ import annotations
+
+
+class TestNcYaml:
+    """Class to test NcYaml from h5yaml.yaml_nc."""
+
+    def test_groups(self: TestNcYaml) -> None:
+        """..."""
+        return
+
+    def test_dimensions(self: TestNcYaml) -> None:
+        """..."""
+        return
+
+    def test_compounds(self: TestNcYaml) -> None:
+        """..."""
+        return
+
+    def test_variables(self: TestNcYaml) -> None:
+        """..."""
+        return
