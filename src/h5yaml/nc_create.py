920225daa3f7bc9f8bc7028b8a69d1ed1aad6cee
Richard van Hees <r.m.van.hees@sron.nl>
2026-03-23 15:15:21 +0100

    Simplified code like class H5Create

--- src/h5yaml/nc_create.py
+++ src/h5yaml/nc_create.py
@@ -177,6 +177,138 @@ class NcCreate:
             comp_t = np.dtype([(k, v[0]) for k, v in val.items()])
             _ = fid.createCompoundType(comp_t, key)
 
+    def __var_scalar(self: NcCreate, fid: Dataset, key: str, val: dict) -> dict:
+        """Return parameters to create a scalar variable.
+
+        Parameters
+        ----------
+        key :  str
+           Name of the variable
+        val :  dict
+           Properties of the variable
+
+        """
+        fillvalue = None
+        if "_FillValue" in val:
+            fillvalue = np.nan if val["_FillValue"] == "NaN" else int(val["_FillValue"])
+
+        return {
+            "varname": key,
+            "datatype": (
+                fid.cmptypes[val["_dtype"]]
+                if val["_dtype"] in fid.cmptypes
+                else val["_dtype"]
+            ),
+            "contiguous": True,
+            "fill_value": fillvalue,
+        }
+
+    def __var_nochunk(self: NcCreate, fid: Dataset, key: str, val: dict) -> dict:
+        """Return parameters to create a variable without chunking.
+
+        Parameters
+        ----------
+        key :  str
+           Name of the variable
+        val :  dict
+           Properties of the variable
+
+        """
+        fillvalue = None
+        if "_FillValue" in val:
+            fillvalue = np.nan if val["_FillValue"] == "NaN" else int(val["_FillValue"])
+
+        n_udim = 0
+        var_dims = []
+        for coord in val["_dims"]:
+            pcoord = PurePosixPath(coord)
+            var_dims.append(pcoord.name if pcoord.is_absolute() else coord)
+            if pcoord.is_absolute():
+                dim_sz = fid[pcoord.parent].dimensions[pcoord.name].size
+            else:
+                dim_sz = fid.dimensions[coord].size
+            n_udim += int(dim_sz == 0)
+
+        # currently, we can not handle more than one unlimited dimension
+        if n_udim > 0:
+            raise KeyError(
+                "you can not create a contiguous dataset with unlimited dimensions."
+            )
+
+        return {
+            "varname": key,
+            "datatype": (
+                fid.cmptypes[val["_dtype"]]
+                if val["_dtype"] in fid.cmptypes
+                else val["_dtype"]
+            ),
+            "dimensions": var_dims,
+            "contiguous": True,
+            "fill_value": fillvalue,
+        }
+
+    def __var_chunked(self: NcCreate, fid: Dataset, key: str, val: dict) -> dict:
+        """Return parameters to create a variable with chunking.
+
+        Parameters
+        ----------
+        key :  str
+           Name of the variable
+        val :  dict
+           Properties of the variable
+
+        """
+        fillvalue = None
+        if "_FillValue" in val:
+            fillvalue = np.nan if val["_FillValue"] == "NaN" else int(val["_FillValue"])
+
+        n_udim = 0
+        var_dims = []
+        for coord in val["_dims"]:
+            pcoord = PurePosixPath(coord)
+            var_dims.append(pcoord.name if pcoord.is_absolute() else coord)
+            if pcoord.is_absolute():
+                dim_sz = fid[pcoord.parent].dimensions[pcoord.name].size
+            else:
+                dim_sz = fid.dimensions[coord].size
+            n_udim += int(dim_sz == 0)
+
+        # currently, we can not handle more than one unlimited dimension
+        if n_udim > 1:
+            raise ValueError(f"{key} has more than one unlimited dimension")
+
+        compression = None
+        complevel = 0
+        # currently only gzip compression is supported
+        if "_compression" in val:
+            compression = "zlib"
+            complevel = val["_compression"]
+
+        ds_chunk = val.get("_chunks")
+        if ds_chunk is not None:
+            ds_chunk = None if isinstance(ds_chunk, bool) else tuple(ds_chunk)
+
+        ds_dtype = (
+            fid.cmptypes[val["_dtype"]]
+            if val["_dtype"] in fid.cmptypes
+            else val["_dtype"]
+        )
+        if "_vlen" in val:
+            if val["_dtype"] in fid.cmptypes:
+                raise ValueError("can not have vlen with compounds")
+            ds_dtype = fid.createVLType(ds_dtype, "phony_vlen")
+            fillvalue = None
+
+        return {
+            "varname": key,
+            "datatype": ds_dtype,
+            "dimensions": var_dims,
+            "fill_value": fillvalue,
+            "compression": compression,
+            "complevel": complevel,
+            "chunksizes": ds_chunk,
+        }
+
     def __variables(self: NcCreate, fid: Dataset) -> None:
         """Add datasets to a netCDF4 product.
 
@@ -186,114 +318,22 @@ class NcCreate:
            netCDF4 Dataset (mode 'r+')
 
         """
-
-        def add_compound_attr() -> None:
-            compound = self.compounds[val["_dtype"]]
-            res = [v[2] for k, v in compound.items() if len(v) == 3]
-            if res:
-                dset.units = [v[1] for k, v in compound.items()]
-                dset.names = res
-            else:
-                dset.names = [v[1] for k, v in compound.items()]
-
         for key, val in self.variables.items():
             pkey = PurePosixPath(key)
             var_grp = fid[pkey.parent] if pkey.is_absolute() else fid
             var_name = pkey.name if pkey.is_absolute() else key
 
-            if val["_dtype"] in fid.cmptypes:
-                is_compound = True
-                ds_dtype = fid.cmptypes[val["_dtype"]]
-            else:
-                is_compound = False
-                ds_dtype = val["_dtype"]
+            # check if dtype of variable is compound
+            is_compound = val["_dtype"] in fid.cmptypes
 
-            fillvalue = None
-            if "_FillValue" in val:
-                fillvalue = (
-                    np.nan if val["_FillValue"] == "NaN" else int(val["_FillValue"])
-                )
-
-            # check for scalar dataset
+            # create variable
             if val["_dims"][0] == "scalar":
-                print(var_name, is_compound, ds_dtype)
-                dset = var_grp.createVariable(
-                    var_name,
-                    ds_dtype,
-                    fill_value=fillvalue,
-                    contiguous=True,
-                )
-                dset.setncatts(
-                    {
-                        k: adjust_attr(ds_dtype, k, v)
-                        for k, v in val.items()
-                        if not k.startswith("_")
-                    }
-                )
-                if is_compound:
-                    add_compound_attr()
-                continue
-
-            compression = None
-            complevel = 0
-            # currently only gzip compression is supported
-            if "_compression" in val:
-                compression = "zlib"
-                complevel = val["_compression"]
-
-            var_dims = []
-            n_udim = 0
-            ds_shape = ()
-            ds_maxshape = ()
-            for coord in val["_dims"]:
-                pcoord = PurePosixPath(coord)
-                var_dims.append(pcoord.name if pcoord.is_absolute() else coord)
-                if pcoord.is_absolute():
-                    dim_sz = fid[pcoord.parent].dimensions[pcoord.name].size
-                else:
-                    dim_sz = fid.dimensions[coord].size
-                n_udim += int(dim_sz == 0)
-                ds_shape += (dim_sz,)
-                ds_maxshape += (dim_sz if dim_sz > 0 else None,)
-
-            # currently, we can not handle more than one unlimited dimension
-            if n_udim > 1:
-                raise ValueError("more than one unlimited dimension")
-
-            if None in ds_maxshape and val.get("_chunks") == "contiguous":
-                raise KeyError(
-                    "you can not create a contiguous dataset with unlimited dimensions."
-                )
-
-            # create the variable
-            if val.get("_chunks") == "contiguous":
-                dset = var_grp.createVariable(
-                    var_name,
-                    ds_dtype,
-                    dimensions=var_dims,
-                    fill_value=fillvalue,
-                    contiguous=True,
-                )
+                dset = var_grp.createVariable(**self.__var_scalar(fid, var_name, val))
+            elif val.get("_chunks") == "contiguous":
+                dset = var_grp.createVariable(**self.__var_nochunk(fid, var_name, val))
             else:
-                ds_chunk = val.get("_chunks")
-                if ds_chunk is not None:
-                    ds_chunk = None if isinstance(ds_chunk, bool) else tuple(ds_chunk)
-                if val.get("_vlen"):
-                    if is_compound:
-                        raise ValueError("can not have vlen with compounds")
-                    ds_dtype = fid.createVLType(ds_dtype, "phony_vlen")
-                    fillvalue = None
+                dset = var_grp.createVariable(**self.__var_chunked(fid, var_name, val))
 
-                dset = var_grp.createVariable(
-                    var_name,
-                    str if val["_dtype"] == "str" else ds_dtype,
-                    dimensions=var_dims,
-                    fill_value=fillvalue,
-                    compression=compression,
-                    complevel=complevel,
-                    chunksizes=ds_chunk,
-                    contiguous=False,
-                )
             dset.setncatts(
                 {
                     k: adjust_attr(val["_dtype"], k, v)
@@ -302,7 +342,13 @@ class NcCreate:
                 }
             )
             if is_compound:
-                add_compound_attr()
+                compound = self.compounds[val["_dtype"]]
+                res = [v[2] for k, v in compound.items() if len(v) == 3]
+                if res:
+                    dset.units = [v[1] for k, v in compound.items()]
+                    dset.names = res
+                else:
+                    dset.names = [v[1] for k, v in compound.items()]
 
     def create(self: NcCreate, filename: Path | str) -> None:
         """Create a netCDF4 file (overwrite if exist).
